b11a0c90b98e4593619bed7eada5eff1f41bc49e
Abhishek Chaturvedi <148352005+abhi1995coder@users.noreply.github.com>
2025-03-11 01:59:20 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const mysql = require("mysql2");
 
 // Create a connection pool
 const pool = mysql.createPool({
+    console.log(process.env.DB_HOST);
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password: process.env.DB_PASSWORD,
